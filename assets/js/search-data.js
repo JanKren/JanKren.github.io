3a358d1c140e54a8a825fd574e94a13412054949
JanKren <krenjan7@gmail.com>
2026-05-19 08:54:48 +0000

    Deploying to gh-pages from @ JanKren/JanKren.github.io@38b54d24dfe5afa73571bb6f1a4b2cda7995db36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,6 +40,13 @@ ninja.data = [{
           window.open("https://github.com/JanKren", "_blank");
         },
       },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
